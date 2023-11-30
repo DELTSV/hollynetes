15455e7e160476029da37bbd81bf420e53d42834
Loïc Vanden Bossche <loloic066@gmail.com>
2023-11-30 12:24:58 +0100

    Fixed dns record

--- packages/infrastructure/project/dns.tf
+++ packages/infrastructure/project/dns.tf
@@ -1,10 +1,9 @@
 data "scaleway_domain_zone" "hollynetes" {
-  domain    = "hollyfilms.fr"
+  domain = "hollynetes.fr"
 }
 
 resource "scaleway_domain_record" "hollynetes" {
   dns_zone = data.scaleway_domain_zone.hollynetes.id
-  name     = "k8s"
   type     = "A"
   data     = kubernetes_ingress_v1.app_ingress.status[0].load_balancer[0].ingress[0].ip
   ttl      = 3600
