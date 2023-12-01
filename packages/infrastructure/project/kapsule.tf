dca1824178cd89bbcc0da1b623639c83e13dcf65
Loïc Vanden Bossche <loloic066@gmail.com>
2023-12-01 19:57:17 +0100

    Renamed api pool to app pool

--- packages/infrastructure/project/kapsule.tf
+++ packages/infrastructure/project/kapsule.tf
@@ -9,16 +9,16 @@ resource "scaleway_k8s_cluster" "main_cluster" {
   depends_on         = [scaleway_vpc_private_network.main_network]
 }
 
-resource "scaleway_k8s_pool" "api_pool" {
+resource "scaleway_k8s_pool" "main_pool" {
   cluster_id = scaleway_k8s_cluster.main_cluster.id
-  name       = "api-pool"
+  name       = "main-pool"
   node_type  = "DEV1-M"
   size       = 1
 }
 
 resource "scaleway_vpc_private_network" "main_network" {
   name = "main-network"
-  tags = ["api", "database", "terraform"]
+  tags = ["terraform", "main"]
 
   ipv4_subnet {
     subnet = "172.16.12.0/22"
